0a76f940578a76ae409889696fdc1949ae553048
阿牛哥 <359945962@qq.com>
2016-11-04 15:01:57 +0800

    添加新的高德地图

---
+++ app/src/main/java/safebox/yiye/com/safebox/utils/ShowDialogUtils.java
@@ -0,0 +1,41 @@
+package safebox.yiye.com.safebox.utils;
+
+import android.content.Context;
+
+import com.dou361.dialogui.config.BuildBean;
+
+/**
+ * Created by aina on 2016/11/4.
+ */
+
+public class ShowDialogUtils {
+//    /**
+//     * md风格竖向加载框  默认白色背景可取消可点击
+//     *
+//     * @param context 上下文
+//     * @param msg     提示文本
+//     */
+//    public static BuildBean showMdLoadingVertical(Context context, CharSequence msg);
+//
+//    /**
+//     * md风格竖向加载框 默认可取消可点击
+//     *
+//     * @param context   上下文
+//     * @param msg       提示文本
+//     * @param isWhiteBg true为白色背景false为灰色背景
+//     */
+//    public static BuildBean showMdLoadingVertical(Context context, CharSequence msg, boolean isWhiteBg)
+//
+//    /**
+//     * md风格竖向加载框
+//     *
+//     * @param context          上下文
+//     * @param msg              提示文本
+//     * @param cancleable       true为可以取消false为不可取消
+//     * @param outsideTouchable true为可以点击空白区域false为不可点击
+//     * @param isWhiteBg        true为白色背景false为灰色背景
+//     */
+//    public static BuildBean showMdLoadingVertical(Context context,
+//                                                  CharSequence msg, boolean cancleable, boolean outsideTouchable, boolean isWhiteBg)
+
+}
